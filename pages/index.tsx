0ad18ca482fa64f4a860044da6b31b805d1190a2
soohyun <soohyun_kim2@tmax.co.kr>
2022-06-24 14:09:25 +0900

    responsive modifier part 2 media query

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,17 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 flex flex-col space-y-5 min-h-screen">
-      <div className="bg-white sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
+    <div className="bg-slate-400 xl:place-items-center py-20 px-20 grid gap-10  lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <div className="bg-white flex flex-col justify-between p-6 rounded-3xl shadow-xl">
         <span className="font-bold text-3xl">Select Item</span>
         <ul>
-          {[1, 2, 3, 4, 5].map(i => (
+          {[1, 2].map(i => (
             <div key={i} className="flex justify-between my-2">
               <span className="text-gray-500">Grey Chair</span>
               <span className="font-semibold">$19</span>
             </div>
           ))}
         </ul>
-        {['a', 'b', 'c', '', 'e'].map((c, i) => (
-          <li key={i} className="bg-red-500 py-2 empty:hidden">
-            {c}
-          </li>
-        ))}
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$38</span>
@@ -27,7 +22,7 @@ const Home: NextPage = () => {
         </button>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
-        <div className="bg-blue-500 p-6 pb-14">
+        <div className="bg-blue-500 landscape:bg-teal-500 portrait:bg-yellow-500 p-6 pb-14 xl:pb-40">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl p-6 bg-white relative -top-5">
@@ -48,7 +43,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-3xl shadow-xl">
+      <div className="bg-white p-10 rounded-3xl shadow-xl lg:col-span-2 xl:col-span-1">
         <div className="flex justify-between items-center mb-5">
           <span>⬅</span>
           <div className="space-x-3">
@@ -82,7 +77,7 @@ const Home: NextPage = () => {
           <button className="bg-blue-500 text-center text-white rounded-lg py-2 px-8 text-xs">Add to cart</button>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-3xl shadow-xl"> </div>
+      {/* <div className="bg-white p-10 rounded-3xl shadow-xl"> </div> */}
     </div>
   );
 };
