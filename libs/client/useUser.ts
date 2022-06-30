5bea0a0f6a2e2e7dcc1d95e01d0470f209ed3ade
soohyun <soohyun_kim2@tmax.co.kr>
2022-06-30 15:12:55 +0900

    useSWR

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,20 +1,17 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 export default function useUser() {
+  const { data, error } = useSWR('/api/users/me');
   const [user, setUser] = useState();
   const router = useRouter();
 
   useEffect(() => {
-    fetch('/api/users/me')
-      .then(res => res.json())
-      .then(data => {
-        if (!data?.ok) {
-          return router.replace('/enter');
-        }
-        setUser(data?.profile);
-      });
-  }, [router]);
+    if (data && !data.ok) {
+      router.replace('/enter');
+    }
+  }, [data, router]);
 
-  return user;
+  return { user: data?.profile, isLoading: !data && !error };
 }
