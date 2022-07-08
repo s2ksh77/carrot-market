cf8a4fe4e25675647e91140947fdada7d8087cb2
soohyun <soohyun_kim2@tmax.co.kr>
2022-07-08 17:32:29 +0900

    image upload with cloudflare

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -18,7 +18,7 @@ async function handler(
   if (req.method === 'POST') {
     const {
       session: { user },
-      body: { email, phone, name },
+      body: { email, phone, name, avatarId },
     } = req;
 
     const userInfo = await client.user.findUnique({
@@ -79,6 +79,16 @@ async function handler(
         },
         data: {
           name,
+        },
+      });
+    }
+    if (avatarId) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          avatar: avatarId,
         },
       });
     }
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -4,7 +4,7 @@ import Input from '@components/input';
 import Layout from '@components/layout';
 import useUser from '@libs/client/useUser';
 import { useForm } from 'react-hook-form';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useMutation from '@libs/client/useMutation';
 import { useRouter } from 'next/router';
 
@@ -12,6 +12,7 @@ interface EditProfileForm {
   email?: string;
   phone?: string;
   name?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -21,6 +22,7 @@ interface EditProfileResponse {
 }
 
 const EditProfile: NextPage = () => {
+  const [avatarPreview, setAvatarPreview] = useState('');
   const router = useRouter();
   const { user } = useUser();
   const {
@@ -29,6 +31,7 @@ const EditProfile: NextPage = () => {
     handleSubmit,
     setError,
     formState: { errors },
+    watch,
   } = useForm<EditProfileForm>();
 
   const [editProfile, { loading, data }] =
@@ -38,15 +41,38 @@ const EditProfile: NextPage = () => {
     if (user?.name) setValue('name', user?.name);
     if (user?.email) setValue('email', user?.email);
     if (user?.phone) setValue('phone', user?.phone);
+    if (user?.avatar)
+      setAvatarPreview(
+        `https://imagedelivery.net/lq0Bbd0nkd16pEanIr4F5Q/${user?.avatar}/avatar`,
+      );
   }, [user, setValue]);
 
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === '' && phone === '' && name === '')
       return setError('formErrors', {
         message: '이름 혹은 이메일이나 핸드폰 번호를 입력하세요.',
       });
-    editProfile({ email, phone, name });
+
+    if (avatar && avatar.length > 0 && user) {
+      // cloudflare url
+      const { uploadURL } = await (await fetch(`/api/files`)).json();
+      console.log(uploadURL);
+
+      const form = new FormData();
+      form.append('file', avatar[0], user?.id + '');
+      const {
+        result: { id },
+      } = await (
+        await fetch(uploadURL, {
+          method: 'POST',
+          body: form,
+        })
+      ).json();
+      editProfile({ email, phone, name, avatarId: id });
+    } else {
+      editProfile({ email, phone, name });
+    }
   };
 
   useEffect(() => {
@@ -61,17 +87,33 @@ const EditProfile: NextPage = () => {
     }
   }, [data, router]);
 
+  const avatar = watch('avatar');
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
+
   return (
     <Layout canGoBack title="프로필 수정">
       <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             변경
             <input
+              {...register('avatar')}
               id="picture"
               type="file"
               className="hidden"
