71b16f38d7afe3889e0373ed98d5949a2e2039cf
soohyun <soohyun_kim2@tmax.co.kr>
2022-07-09 19:33:10 +0900

    nextjs image component

--- components/item.tsx
+++ components/item.tsx
@@ -1,3 +1,5 @@
+import { getImageSrc } from '@libs/client/utils';
+import Image from 'next/image';
 import Link from 'next/link';
 
 interface ItemProps {
@@ -6,6 +8,7 @@ interface ItemProps {
   price: number;
   comments: number;
   hearts: number;
+  image: string;
 }
 
 export default function Item({
@@ -14,12 +17,22 @@ export default function Item({
   comments,
   hearts,
   id,
+  image,
 }: ItemProps) {
   return (
     <Link href={`/products/${id}`}>
       <a className="flex px-4 pt-5 cursor-pointer justify-between">
         <div className="flex space-x-4">
-          <div className="w-20 h-20 bg-gray-400 rounded-md" />
+          {image ? (
+            <Image
+              width={'80px'}
+              height={'80px'}
+              src={getImageSrc(image, 'public')}
+              className=" bg-gray-400 rounded-md"
+            />
+          ) : (
+            <div className="w-20 h-20 bg-gray-400 rounded-md" />
+          )}
           <div className="pt-2 flex flex-col">
             <h3 className="text-sm font-medium text-gray-900">{title}</h3>
             <span className="font-medium mt-1 text-gray-900">\{price}</span>
