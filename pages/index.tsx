ca0aa2b83fc59e0fdddf198440d208bb20025407
soohyun <soohyun_kim2@tmax.co.kr>
2022-07-22 14:50:09 +0900

    ssr + swr

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Item from '@components/item';
 import Layout from '@components/layout';
 import useUser from '@libs/client/useUser';
 import Head from 'next/head';
-import useSWR from 'swr';
+import useSWR, { SWRConfig } from 'swr';
 import { Product } from '@prisma/client';
 import client from '@libs/server/client';
 
@@ -19,24 +19,24 @@ interface ProductsResponse {
   products: ProductWithCount[];
 }
 
-const Home: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
+const Home: NextPage = () => {
   const { user, isLoading } = useUser();
-  // const { data } = useSWR<ProductsResponse>('/api/products');
+  const { data } = useSWR<ProductsResponse>('/api/products');
   return (
     <Layout title="홈" hasTabBar>
       <Head>
         <title>Home</title>
       </Head>
       <div className="flex flex-col space-y-5 divide-y">
-        {products?.map(product => (
+        {data?.products?.map(product => (
           <Item
             id={product.id}
             key={product.id}
             title={product.name}
             price={product.price}
             image={product.image}
-            comments={product._count?.chats}
-            hearts={product._count?.favorites}
+            comments={product._count?.chats | 0}
+            hearts={product._count?.favorites | 0}
           />
         ))}
         <FloatingButton href="/products/upload">
@@ -61,6 +61,23 @@ const Home: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
   );
 };
 
+const Page: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          '/api/products': {
+            ok: true,
+            products,
+          },
+        },
+      }}
+    >
+      <Home />
+    </SWRConfig>
+  );
+};
+
 export async function getServerSideProps() {
   const products = await client.product.findMany({});
   return {
@@ -70,4 +87,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default Home;
+export default Page;
