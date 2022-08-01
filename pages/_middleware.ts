090460f92e1d9e0c846cd7d34c83e31002bfe2e4
soohyun <soohyun_kim2@tmax.co.kr>
2022-08-01 16:21:44 +0900

    server component in nextjs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,12 +3,12 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const { pathname, origin } = req.nextUrl;
-  if (req.ua?.isBot) {
-    return new Response("Plz don't be a bot be a human", { status: 403 });
-  }
-  // if (!req.url.includes('/api')) {
-  //   if (!req.url.includes('/enter') && !req.cookies.carrotmarket) {
-  //     return NextResponse.redirect(`${origin}/enter`);
-  //   }
+  // if (req.ua?.isBot) {
+  //   return new Response("Plz don't be a bot be a human", { status: 403 });
   // }
+  if (!req.url.includes('/api')) {
+    if (!req.url.includes('/enter') && !req.cookies.carrotmarket) {
+      return NextResponse.redirect(`${origin}/enter`);
+    }
+  }
 }
